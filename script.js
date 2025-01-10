3849b3570225f38ee89d7fa4c29e562a6d89be03
Sarthak900 <sarthakpandit1828@gmail.com>
2025-01-10 12:27:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('scroll', () => {
+    const navbar = document.getElementById('navbar');
+    if (window.scrollY > 60) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
